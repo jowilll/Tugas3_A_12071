13928545764f848745856543046ef839e0127483
will69 <220712071@students.uajy.ac.id>
2024-10-05 16:11:49 +0700

    Update Tugas_Modul3_A_jowil_12071.py

--- Tugas_Modul3_A_jowil_12071.py
+++ Tugas_Modul3_A_jowil_12071.py
@@ -80,8 +80,7 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color: #0073e6;'>Data yang diupload</h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'C:\Users\Lenovo\Documents\Kuliah - UAJY\Semester V - 2024-2025\Pembelajaran mesin dan Pembelajaran Mendalam - A\Modul 03\Tugas3_A_12071'
-    model_path = os.path.join(model_directory, r'SVR_IPK_model.pkl')
+    model_path = 'SVR_IPK_model.pkl'
 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
